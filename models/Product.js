e9417d60e7dc926f9480994b4de2a11d7c02c85c
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-27 16:41:52 -0700

    finish product model

--- models/Product.js
+++ models/Product.js
@@ -23,17 +23,24 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      // validate that the value is a decimal
+      validate: {
+        isDecimal: true
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      // validate that the value is numeric
+      validate: {
+        isNumeric: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
-      // references category model's id
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     },
   },
   {
