75de23216d690227efd8393c0ba7e15feb16de46
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-27 16:32:04 -0700

    product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references the product models id
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references the tag model's id
+    },
   },
   {
     sequelize,
