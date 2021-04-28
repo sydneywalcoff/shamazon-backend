0e3ce74bea6f93e557782091beb1ce60f57c31b9
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-27 17:19:36 -0700

    fixed error

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,7 +49,7 @@ router.post('/', (req, res) => {
   Category.create({
     category_name: req.body.category_name
   })
-  .then(dbCategoryData => res.json(dbCategoryData)
+  .then(dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
